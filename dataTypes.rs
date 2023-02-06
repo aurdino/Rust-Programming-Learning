f9dc2646df83acea016577f708662b5b22e65d30
Vishal Kumar <vr16796@gmail.com>
2023-02-06 12:36:21 +0530

    Create dataTypes.rs

---
+++ dataTypes.rs
@@ -0,0 +1,11 @@
+fn main()
+{
+	let mut x = 5 ;
+	println!("The value of x is : {x}");
+	x = 6 ;
+	println!("Now the changed value of x is : {x}");
+	const NAME:&str = "Vishal";
+	println!("My name is :  {NAME}");
+	//NAME = "Rajak";
+	println!("Last name is : {NAME}");
+}
